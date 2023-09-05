cd8eac9e3d4dd93da77ac74c56cd6e532bb972fd
Rahul Bisht <bishtrahul.1704@gmail.com>
2023-09-05 22:44:21 +0530

    displayed shows and actors data

---
+++ src/components/shows/ShowsGrid.jsx
@@ -0,0 +1,17 @@
+import ShowCard from './ShowCard';
+const ShowsGrid = ({ shows }) => {
+  return (
+    <div>
+      {shows.map(data => (
+        <ShowCard
+          key={data.show.id}
+          id={data.show.id}
+          name={data.show.name}
+          image={data.show.image ? data.show.image.medium : '/notFound.png'}
+          summary={data.show.summary}
+        />
+      ))}
+    </div>
+  );
+};
+export default ShowsGrid;

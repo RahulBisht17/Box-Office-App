e46814fcf4371e3c6a4147bd8f5d2573a5b1f410
Rahul Bisht <bishtrahul.1704@gmail.com>
2023-09-08 21:41:04 +0530

    Read more button to new tab now,added go back to home

--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 const ShowCard = ({ name, image, id, summary }) => {
   const summaryStripped = summary
     ? summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '')
@@ -11,7 +10,9 @@ const ShowCard = ({ name, image, id, summary }) => {
       <h1>{name}</h1>
       <p>{summaryStripped}</p>
       <div>
-        <Link to={`/show/${id}`}>Read More</Link>
+        <a href={`/show/${id}`} target="_blank" rel="noreferrer">
+          Read More
+        </a>
         <button type="button">Start Me</button>
       </div>
     </div>
